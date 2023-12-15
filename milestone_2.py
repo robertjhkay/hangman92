22a5a225a6cdf371439643e192b9cf40853d035c
Robert <robertkay50@hotmail.com>
2023-12-15 15:29:11 +0000

    Ask the user for an input

--- milestone_2.py
+++ milestone_2.py
@@ -2,3 +2,9 @@ import random
 word_list = ['apple', 'pear', 'orange','banana', 'kiwi']
 word = random.choice(word_list)
 print(word)
+
+guess = input("Please choose a single letter ")
+
+if len(guess) == 1:
+    print("Good guess!")
+else: print("Oops! That is not a valid input.")
